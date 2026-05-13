e54340f21d313d348709c6b705094c0373aae688
三ツ矢昂平 <chaneko914@icloud.com>
2026-05-13 22:31:52 +0900

    シングルページ（news）PHP実装

--- single-news.php
+++ single-news.php
@@ -4,23 +4,39 @@
   <?php if (have_posts()) : ?>
   <?php while (have_posts()) : the_post(); ?>
 
+  <?php
+    $terms = get_the_terms(get_the_ID(), 'genre');
+
+    if ($terms && !is_wp_error($terms)) {
+      $term_names = [];
+      foreach ($terms as $term) {
+        $term_names[] = strtoupper($term->name);
+      }
+    }
+
+  ?>
+
   <div class="single-inner">
     <a href="<?php echo esc_url( home_url('/works/') ); ?>" class="single-prev-btn">すべての作品</a>
     <div class="single-title-wrap">
-      <p class="single-tax">WEB DESIGN / DEVELOPMENT</p>
-      <h2 class="signle-title">和菓子屋HP制作</h2>
-      <time datetime="2024-08-16" class="single-date">2024-08-16</time>
+      <p class="single-tax"><?php echo implode(' / ', $term_names); ?></p>
+      <h2 class="signle-title"><?php echo esc_html(get_the_title()); ?></h2>
+      <time datetime="<?php echo get_the_date('Y-m-d'); ?>" class="single-date"><?php echo get_the_date('Y-m-d'); ?></time>
     </div>
-    <div class="single-main-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/device_mockup.png" alt=""></div>
+    <div class="single-main-img"><img src="<?php echo esc_url(get_field('eye_catch')); ?>" alt=""></div>
     <div class="single-news-article">
       <?php the_content(); ?>
       <div class="single-news-tag-wrap">
         <p class="single-news-tag-title">Tags</p>
         <div class="single-news-tag-list">
-          <span class="single-news-tag">Web Design</span>
-          <span class="single-news-tag">Trends</span>
-          <span class="single-news-tag">UI/UX</span>
-          <span class="single-news-tag">2026</span>
+          <?php
+            $terms = get_the_terms(get_the_ID(), 'tags');
+
+            if($terms && !is_wp_error($terms)) :
+              foreach($terms as $term) :
+          ?>
+            <span class="single-news-tag"><?php echo esc_html($term->name); ?></span>
+          <?php endforeach; endif; ?>
         </div>
       </div>
     </div>
@@ -31,39 +47,43 @@
   <nav class="single-news-connection-wrap">
     <p class="single-news-connection-title">関連記事</p>
     <ul class="single-news-connection-list">
-      <li class="single-news-connection-item">
-        <a href="">
-          <div class="single-news-connection-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/device_mockup.png" alt=""></div>
-          <div class="single-news-connection-item-tax-wrap">
-            <p class="single-news-connection-item-tax">Web Design</p>
-            <time datetime="2026-02-14" class="single-news-connection-item-date">2026-02-14</time>
-          </div>
-          <p class="single-news-connection-item-title">丸富庵清月</p>
-          <p class="single-news-connection-item-txt">モダンなECサイトのデザインと開発</p>
-        </a>
-      </li>
-      <li class="single-news-connection-item">
-        <a href="">
-          <div class="single-news-connection-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/device_mockup.png" alt=""></div>
-          <div class="single-news-connection-item-tax-wrap">
-            <p class="single-news-connection-item-tax">Web Design</p>
-            <time datetime="2026-02-14" class="single-news-connection-item-date">2026-02-14</time>
-          </div>
-          <p class="single-news-connection-item-title">丸富庵清月</p>
-          <p class="single-news-connection-item-txt">モダンなECサイトのデザインと開発</p>
-        </a>
-      </li>
-      <li class="single-news-connection-item">
-        <a href="">
-          <div class="single-news-connection-item-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/device_mockup.png" alt=""></div>
-          <div class="single-news-connection-item-tax-wrap">
-            <p class="single-news-connection-item-tax">Web Design</p>
-            <time datetime="2026-02-14" class="single-news-connection-item-date">2026-02-14</time>
-          </div>
-          <p class="single-news-connection-item-title">丸富庵清月</p>
-          <p class="single-news-connection-item-txt">モダンなECサイトのデザインと開発</p>
-        </a>
-      </li>
+      <?php
+        $current_post_id = get_the_ID(); // 今表示してる投稿のIDを取得
+
+        $args = array(
+          'post_type' => 'news', 
+          'posts_per_page' => 3,
+          'post__not_in' => array($current_post_id), // 自分自身を除外！
+          'orderby' => 'date',
+          'order' => 'DESC'
+        );
+
+        $news_array = new WP_Query($args);
+      
+        if ($news_array->have_posts()) :
+          while ($news_array->have_posts()) :
+            $news_array->the_post();
+            $news_img = get_field('eye_catch');
+
+            $genre = '';
+            $terms = get_the_terms(get_the_ID(), 'genre');
+
+            if ($terms && !is_wp_error($terms)) {
+              $genre = implode(' / ', wp_list_pluck($terms, 'name'));
+            }
+      ?>
+        <li class="single-news-connection-item">
+          <a href="<?php the_permalink(); ?>">
+            <div class="single-news-connection-item-img"><img src="<?php echo esc_url($news_img); ?>" alt=""></div>
+            <div class="single-news-connection-item-tax-wrap">
+              <p class="single-news-connection-item-tax"><?php echo $genre ?></p>
+              <time datetime="<?php echo get_the_date('Y-m-d'); ?>" class="single-news-connection-item-date"><?php echo get_the_date('Y-m-d'); ?></time>
+            </div>
+            <p class="single-news-connection-item-title"><?php echo esc_html(get_the_title()); ?></p>
+            <p class="single-news-connection-item-txt"><?php echo wp_strip_all_tags(get_the_content()); ?></p>
+          </a>
+        </li>
+      <?php endwhile; endif; ?>
     </ul>
   </nav>
 </div>
